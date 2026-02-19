d2cae7831bb0814956e1f83bf09c7264e86c28ee
srujan <codewithsrujan@gmail.com>
2026-02-19 19:59:09 +0530

    Fix compilation errors: added javax.annotation-api and Lombok dependencies, fixed BillingGrpcService

--- PatientManagment/billing-service/src/main/java/com/example/demo/grpc/BillingGrpcService.java
+++ PatientManagment/billing-service/src/main/java/com/example/demo/grpc/BillingGrpcService.java
@@ -5,17 +5,23 @@ import billing.BillingResponse;
 import billing.BillingServiceGrpc;
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @GrpcService
 public class BillingGrpcService extends BillingServiceGrpc.BillingServiceImplBase {
+    private static final Logger log = LoggerFactory.getLogger(BillingGrpcService.class);
 
     @Override
     public void createBillingAccount(BillingRequest request, StreamObserver<BillingResponse> responseObserver) {
-      log.info("Creating BillingRequest {}",BillingRequest.toString());
+      log.info("Creating BillingRequest {}", request.toString());
 
-      BillingResponse response=BillingResponse.toBuilder().setAccountId("1234").setStatus("Active").build();
-      responseObserver.Onnext(patient);
-      responseObserver.OnCompleted();
+      BillingResponse response = BillingResponse.newBuilder()
+          .setAccountId("1234")
+          .setStatus("Active")
+          .build();
+      responseObserver.onNext(response);
+      responseObserver.onCompleted();
         
     }
 }
